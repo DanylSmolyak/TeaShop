ab79c81875125428b041b3b93553c8bc61eea741
Smolyak Danylo <smolakdanil35@gmail.com>
2024-05-06 22:39:42 +0300

    -Adding Repository Pattern -Seed data -Interfaces -Configuring the migrations

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 using Core.Entities;
 
@@ -10,4 +11,14 @@ public class StoreContext : DbContext
     }
 
     public DbSet<Product> Products { get; set; }
+
+    public DbSet<ProductType> ProductTypes { get; set; }
+    
+    public DbSet<ProductBrand> ProductBrands { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
 }
